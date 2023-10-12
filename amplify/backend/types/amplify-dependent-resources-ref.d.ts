34032b826a41b11102f8f59a01d8eee22b09cd9f
João Madeira Neto <suporte@escritoriolucrativo.com>
2023-10-12 03:48:03 -0300

    Primeiro commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "nextamplified": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "nextamplifieda7c22756": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
---
+++ pages/_app.js
@@ -0,0 +1,5 @@
+import "@aws-amplify/ui-react/styles.css";
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
